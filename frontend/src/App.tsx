528219e21a6d6f1f75ce72e17e0e2d6b6ed349b7
Eduardo <contatoeduardorezes@gmail.com>
2022-07-16 23:09:48 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
+import Header from "./components/Header"
 
 function App() {
-  return(
+  return (
     <>
-      <h1>Ola Mundo!</h1>
-      <NotificationButton />
+      <Header></Header>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
